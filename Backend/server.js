0378ef85df85422438b09c562d472434bab8d991
dessymol <dessymoldixon@gmail.com>
2026-02-25 21:37:47 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,16 @@ const contactRoutes = require("./Routes/contactRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://dessyportfolio.onrender.com',
+    'http://localhost:5000',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 if (process.env.MONGO_URI) {
