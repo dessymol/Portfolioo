b2bc2fab475f90a253c940685a2142bd7db80691
dessymol <dessymoldixon@gmail.com>
2026-02-25 21:48:26 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -8,15 +8,22 @@ const contactRoutes = require("./Routes/contactRoutes");
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS 
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['https://dessyportfolio.onrender.com', 'http://localhost:3000', 'http://localhost:5173'];
+
 app.use(cors({
-  origin: [
-    'https://dessyportfolio.onrender.com',
-    'http://localhost:5000',
-    'http://localhost:5173'
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
 }));
 app.use(express.json());
 
